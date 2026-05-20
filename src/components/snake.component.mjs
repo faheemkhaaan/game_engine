708f808eafd4f2ccbaaf99cf0e43b95ce040f2aa
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-05-20 09:53:51 +0500

    Completed the initial snake component

--- src/components/snake.component.mjs
+++ src/components/snake.component.mjs
@@ -5,5 +5,10 @@
 export class SnakeComponent {
     constructor() {
 
+        this.head = null;
+        this.leftEye = 0;
+        this.rightEye = 0;
+        this.segments = [];
+        this.snakeSkinVerticies = [];
     }
 }
